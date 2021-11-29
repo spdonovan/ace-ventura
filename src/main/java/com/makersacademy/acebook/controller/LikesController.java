0c57f0a28829c3b3d1ed8ff2d088d2ef1a00f7b3
Jenaro Garcia <jjegarcia@hotmail.com>
2021-11-29 18:15:53 +0000

    WIP getting likes callback and redirecting to posts

---
+++ src/main/java/com/makersacademy/acebook/controller/LikesController.java
@@ -0,0 +1,52 @@
+package com.makersacademy.acebook.controller;
+
+import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.model.PostList;
+import com.makersacademy.acebook.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+@Controller
+public class LikesController {
+
+
+    @Autowired
+    PostRepository repository;
+    PostList postArrayList = new PostList();
+
+    @GetMapping("/likes")
+    public String index(Model model) {
+        System.out.println("----------------->index");
+        postArrayList.setList(repository.findAll());
+        model.addAttribute("posts", postArrayList.postArrayList);
+        model.addAttribute("post", new Post());
+        model.addAttribute("showLogout", true);
+        return "posts/index";
+    }
+
+    @PostMapping("/likes")
+    public RedirectView create(@ModelAttribute Post post) {
+        System.out.println("------------------->create");
+//        String username;
+//        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        username = principal instanceof UserDetails ? ((UserDetails) principal).getUsername() : principal.toString();
+//        post.populate(post.getContent(), LocalDateTime.now(), username, 0);
+//        System.out.printf(username);
+//        repository.save(post);
+        return new RedirectView("/posts");
+    }
+}
+
+
